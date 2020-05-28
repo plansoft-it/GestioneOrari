e62c47773e0478b45fad163649600e57ccf7d68e
Antonio Caia <antonio.caia96@gmail.com>
2020-05-28 17:02:29 +0200

    Update (Row.java): added links to documentation

--- GestioneOrari/src/row/Row.java
+++ GestioneOrari/src/row/Row.java
@@ -5,6 +5,10 @@ import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+//https://projectlombok.org
+//https://projectlombok.org/api/lombok/AllArgsConstructor.html
+//https://projectlombok.org/api/lombok/Data.html
+
 @AllArgsConstructor
 public @Data class Row {
 	
