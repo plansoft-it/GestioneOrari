4b0cb36b2fb6621e60f5baffb796c8ec721be513
Antonio Caia <antonio.caia96@gmail.com>
2020-05-30 11:49:24 +0200

    Fix (Parser.java): solved buffer leak

--- GestioneOrari/src/Parser/Parser.java
+++ GestioneOrari/src/Parser/Parser.java
@@ -23,37 +23,36 @@ public class Parser {
 	public void parse() throws IOException {
 		int lineNumber = 0;
 		File file = new File(filePath);
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String rawRow = br.readLine();
-		while (rawRow != null) {
-			lineNumber++;
-			String[] subStrings = rawRow.split(" *\\| *");
-			try {
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String rawRow = br.readLine();
+			while (rawRow != null) {
+				lineNumber++;
+				String[] subStrings = rawRow.split(" *\\| *");
 				Date date = checkDate(subStrings[0]);
 				String startHour = checkTime(subStrings[1]);
 				String endHour = checkTime(subStrings[2]);
 				String order = subStrings[3];
-				String note = subStrings[4];
-			} catch (WrongFormatException | ParseException e) {
-				throw new WrongFormatException("Error in line: " + lineNumber, e);
-			} 
-			// elaborato.addLine(); 
-			rawRow = br.readLine();
+				String note = (subStrings.length == 5) ? subStrings[4] : "";
+				// elaborato.addLine();
+				System.out.println(date + "#" + startHour + "#" + endHour + "#" + order + "#" + note);
+				rawRow = br.readLine();
+			}
+		} catch (WrongFormatException | ParseException e) {
+			throw new WrongFormatException("Error in line: " + lineNumber, e);
 		}
-		br.close();
 	}
 
 	private Date checkDate(String date) throws WrongFormatException, ParseException {
-		if (date.matches("(20[2-9][0-9]|[2-9][0-9])-([1-9]|0[1-9]|1[0-2])-([0-2][0-9]|3[0-1]|[1-9])")) 
+		if (date.matches("(20[2-9][0-9]|[2-9][0-9])-([1-9]|0[1-9]|1[0-2])-([0-2][0-9]|3[0-1]|[1-9])"))
 			return new SimpleDateFormat("yyyy-MM-dd").parse(date);
-		
-		throw new WrongFormatException("Wrong date: " + date + ". Rigth format: <yyyy-MM-dd>");
+
+		throw new WrongFormatException("Wrong date: " + date);
 	}
 
 	private String checkTime(String time) throws WrongFormatException {
 		if (time.matches("([0-9]|1[0-9]|2[0-3]):([0-5][0-9]|[0-9])"))
 			return time;
-		throw new WrongFormatException("Wrong time: " + time + ". Rigth format: <HH:mm>");
+		throw new WrongFormatException("Wrong time: " + time);
 	}
 
 	// restituire il numero di ore del giorno
