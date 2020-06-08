5c2670811b88b90f6ebebf310266113668cd947c
scrivereleonardo <studente.scrivere.leonardo@gmail.com>
2020-06-08 17:33:02 +0200

    Funzione getTotalWorkHours aggiunta (conta le ore totali)

--- GestioneOrari/src/Parser/Parser.java
+++ GestioneOrari/src/Parser/Parser.java
@@ -1,6 +1,12 @@
 package Parser;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import Elaborato.ProcessedFile;
+import errors.WrongFormatException;
 
 public class Parser {
 
@@ -27,8 +33,19 @@ public class Parser {
 	}
 
 	// restituire il numero di ore totali del file
-	public float getTotalWorkHours() {
-		return 0;
+	public float getTotalWorkHours() throws WrongFormatException, ParseException {
+		float oreLavorate = 0;
+		for (int i = 0; i <processedFile.getRows().size(); i++) {
+					
+				SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+		    	Date date1 = format.parse(processedFile.getRows().get(i).getStartHour());
+		    	Date date2 = format.parse(processedFile.getRows().get(i).getEndHour());
+		    	float difference = TimeUnit.MILLISECONDS.toMinutes(date2.getTime() - date1.getTime()); 			    	
+		    	oreLavorate = oreLavorate + difference;
+			
+			
+		}
+		return oreLavorate/60;
 	}
 
 }
