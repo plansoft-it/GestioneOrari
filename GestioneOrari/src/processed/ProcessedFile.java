da1b454979ed085400d5d3013e49484adbb832c2
Antonio Caia <antonio.caia96@gmail.com>
2020-06-11 10:55:17 +0200

    Refactor (ProcessedFile): bow calcolate hours/day and hours/order
    Processed file now calculate the total working hours for each working day and each order

--- GestioneOrari/src/processed/ProcessedFile.java
+++ GestioneOrari/src/processed/ProcessedFile.java
@@ -1,22 +1,54 @@
 package processed;
 
+
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
+import lombok.Getter;
 import row.Row;
 
 public class ProcessedFile {
-	private List<Row> rows;
+	private @Getter HashMap<String, Float> orderHours;
+	private @Getter HashMap<Date, Float> dailyHours;
+	private @Getter float totalWorkOurs;
 	
-	public ProcessedFile() {
-		rows = new ArrayList<>();
+	private @Getter List<Row> rows;
+
+	public ProcessedData() {
+		orderHours = new HashMap<String, Float>();
+		dailyHours = new HashMap<Date, Float>();
+		totalWorkOurs = 0;
+		
+		rows = new ArrayList<Row>();
 	}
-	
-	public void addRowToRows(Row row) {
-		rows.add(row);
+
+
+	public void updateData(Row row) {
+		float hours = getHoursAmmount(row.getStartHour(), row.getEndHour());
+		updateOrderHours(row.getOrder(), hours);
+		updateDailyHours(row.getDate(), hours);
+		updateTotalHours(hours);
+		
+		if (!rows.contains(row)) rows.add(row);
 	}
-	
-	public List<Row> getRows(){
-		return rows;
+
+	private void updateOrderHours(String order, float hours) {
+		orderHours.put(order, orderHours.getOrDefault(order, Float.valueOf(0)) + hours);
+	}
+
+	private void updateDailyHours(Date date, float hours) {
+		dailyHours.put(date, dailyHours.getOrDefault(date, Float.valueOf(0)) + hours);
+	}
+
+	private void updateTotalHours(float hours) {
+		totalWorkOurs += hours;
+	}
+
+	private float getHoursAmmount(String startHour, String endHour) {
+		// TODO 
+		// Calcolare la differenza in ore e minuti tra startHours e end Hours
+		return 1;
 	}
 }
