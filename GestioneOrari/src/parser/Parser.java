16ed39ac853a1b0d8a0d6820c51cbdb78c18843d
Antonio Caia <antonio.caia96@gmail.com>
2020-06-11 11:11:38 +0200

    Update (Parser): method parse done

--- GestioneOrari/src/parser/Parser.java
+++ GestioneOrari/src/parser/Parser.java
@@ -1,34 +1,61 @@
-package Parser;
+package parser;
 
-import Elaborato.ProcessedFile;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+
+import errors.WrongFormatException;
+import processed.ProcessedFile;
+import row.Row;
 
 public class Parser {
 
 	private String filePath;
 	private ProcessedFile processedFile;
 
-	public Parser(String filePath) {
-		this.filePath = filePath;
-		processedFile = new ProcessedFile();
+	public Parser(String file, ProcessedFile processedFile) {
+		this.filePath = file;
+		this.processedFile = processedFile;
 	}
 
 	// esegue il parse del file e inizializza elaborato
-	public void parse() {
+	public void parse() throws IOException {
+		int lineNumber = 0;
+		File file = new File(filePath);
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String rawRow = br.readLine();
+			while (rawRow != null) {
+				lineNumber++;
+				String[] subStrings = rawRow.split(" *\\| *");
+				Date date = FormatChecker.checkDate(subStrings[0]);
+				String startHour = FormatChecker.checkTime(subStrings[1]);
+				String endHour = FormatChecker.checkTime(subStrings[2]);
+				String order = subStrings[3];
+				String note = (subStrings.length == 5) ? subStrings[4] : "";
+				processedFile.updateData(new Row(date, startHour, endHour, order, note));
+				rawRow = br.readLine();
+			}
+		} catch (WrongFormatException | ParseException e) {
+			throw new WrongFormatException("Error in line: " + lineNumber, e);
+		}
 	}
 
 	// restituire il numero di ore del giorno
-	public float getDailyWorkHours(String data) {
-		return 0;
+	public float getDailyWorkHours(Date date) {
+		return processedFile.getDailyHours().get(date);
 	}
 
 	// restituire il numero di ore della commessa
 	public float getOrderWorkHours(String commessa) {
-		return 0;
+		return processedFile.getOrderHours().get(commessa);
 	}
 
 	// restituire il numero di ore totali del file
 	public float getTotalWorkHours() {
-		return 0;
+		return processedFile.getTotalWorkOurs();
 	}
 
 }
