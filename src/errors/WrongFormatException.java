b90f2353cd7e76c318d089006bb206deca212801
MariusLovesPizza <marius.coanta@gmail.com>
2020-06-20 11:49:04 +0200

    Fix (WrongFormatException.java): add serialVersionUID
    Close #34

--- src/errors/WrongFormatException.java
+++ src/errors/WrongFormatException.java
@@ -3,11 +3,14 @@ package errors;
 import java.io.IOException;
 
 public class WrongFormatException extends IOException{
-	    public WrongFormatException(String errorMessage) {
-	        super(errorMessage);
-	    }
-	    
-	    public WrongFormatException(String errorMessage, Throwable errorCause) {
-	        super(errorMessage, errorCause);
-	    }
+	/** serial version UID */
+	private static final long serialVersionUID = 1L;
+
+	public WrongFormatException(String errorMessage) {
+		super(errorMessage);
+	}
+
+	public WrongFormatException(String errorMessage, Throwable errorCause) {
+		super(errorMessage, errorCause);
+	}
 }
